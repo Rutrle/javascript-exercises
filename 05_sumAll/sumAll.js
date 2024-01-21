43125c3e8dbf61f03c01f54ee354b0fab8312291
Jakub Rutrle <jakubrutrle@gmail.com>
2024-01-21 19:24:08 +0100

    5th js exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    if (lowerBound <0 || upperBound < 0 || typeof upperBound != "number" ||typeof lowerBound != "number"){
+        return "ERROR"
+    }
+    if (lowerBound>upperBound){
+        [lowerBound, upperBound] = [upperBound, lowerBound]
+    }
+    let result = 0
+
+    for (let i=lowerBound; i<upperBound+1;i++){
+        result = result + i;
+    }
+
+    return result
 
 };
 
