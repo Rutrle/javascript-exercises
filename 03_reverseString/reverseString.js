83a5415468eeb984e65f369726bbb4ba2ed6e2dd
Jakub Rutrle <jakubrutrle@gmail.com>
2024-01-21 02:27:46 +0100

    third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let stringReverse = ""
+    for (let i=inputString.length-1; i>=0; i--){
+        stringReverse=stringReverse+inputString.charAt(i)
+    }
 
+    return stringReverse
 };
 
 // Do not edit below this line
